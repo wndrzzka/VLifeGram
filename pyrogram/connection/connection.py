f3e895ea6f69ba7bf9069b3f1f8cadea6dd990b1
𝗪𝗔𝗡𝗡𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 <165775366+wndrzzka@users.noreply.github.com>
2025-11-24 22:48:03 +0700

    Update

---
+++ pyrogram/connection/connection.py
@@ -0,0 +1,89 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+import asyncio
+import logging
+from typing import TYPE_CHECKING
+
+from pyrogram.session.internals import DataCenter
+
+from .transport import TCP, TCPAbridged
+
+if TYPE_CHECKING:
+    from .transport.tcp.tcp import Proxy
+
+log = logging.getLogger(__name__)
+
+
+class Connection:
+    MAX_CONNECTION_ATTEMPTS = 3
+
+    def __init__(
+        self,
+        dc_id: int,
+        test_mode: bool,
+        ipv6: bool,
+        proxy: Proxy,
+        media: bool = False,
+        protocol_factory: type[TCP] = TCPAbridged,
+    ) -> None:
+        self.dc_id = dc_id
+        self.test_mode = test_mode
+        self.ipv6 = ipv6
+        self.proxy = proxy
+        self.media = media
+        self.protocol_factory = protocol_factory
+
+        self.address = DataCenter(dc_id, test_mode, ipv6, media)
+        self.protocol: TCP | None = None
+
+    async def connect(self) -> None:
+        for i in range(Connection.MAX_CONNECTION_ATTEMPTS):
+            self.protocol = self.protocol_factory(ipv6=self.ipv6, proxy=self.proxy)
+
+            try:
+                log.info("Connecting...")
+                await self.protocol.connect(self.address)
+            except OSError as e:
+                log.warning("Unable to connect due to network issues: %s", e)
+                await self.protocol.close()
+                await asyncio.sleep(1)
+            else:
+                log.info(
+                    "Connected! %s DC%s%s - IPv%s",
+                    "Test" if self.test_mode else "Production",
+                    self.dc_id,
+                    " (media)" if self.media else "",
+                    "6" if self.ipv6 else "4",
+                )
+                break
+        else:
+            log.warning("Connection failed! Trying again...")
+            raise ConnectionError
+
+    async def close(self) -> None:
+        await self.protocol.close()
+        log.info("Disconnected")
+
+    async def send(self, data: bytes) -> None:
+        await self.protocol.send(data)
+
+    async def recv(self) -> bytes | None:
+        return await self.protocol.recv()
